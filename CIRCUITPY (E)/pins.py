7297258d75db8fe85568aa4e66186863ec95f520
Zoestra <zoe.ophelia.hammer@gmail.com>
2023-09-17 20:18:36 -0700

    added keyboard.toml

---
+++ CIRCUITPY (E)/pins.py
@@ -0,0 +1,50 @@
+pin = {
+    "row1": "GP0",
+    "row2": "GP1",
+    "row3": "GP2",
+    "row4": "GP3",
+    "row5": "GP4",
+    "row6": "GP5",
+    "row7": "GP6",
+
+    "col1": "0000",
+    "col2": "1000",
+    "col3": "0100",
+    "col4": "1100",
+    "col5": "0010",
+    "col6": "1010",
+    "col7": "1110",
+    "col8": "0001",
+    "col9": "1001",
+
+    "led_out": "GP7",
+
+    "e0a": "GP28",
+    "e0b": "GP27",
+    "e1a": "GP2",
+    "e1b": "GP13",
+    "e2a": "GP14",
+    "e2b": "GP15",
+
+    "scl": " GP17",
+    "sda": "GP16",
+
+    "com": "GP22",
+    "s0": "GP21",
+    "s1": "GP20",
+    "s2": "GP19",
+    "s3": "GP18",
+
+    "lcd_dc": "GP8",
+    "lcd_cs": "GP9",
+    "lcd_clk": "GP10",
+    "lcd_din": "GP11",
+    "lcd_rst": "GP12",
+    "lcd_bl": "GP25"
+}
+
+class Mux:
+    def __init__(self)
+
+    @classmethod
+    def CD74HC4067M(cls, com, s0, s1, s2, s3)
