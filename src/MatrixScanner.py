6259eb44117a8712f0df440de8b74bd29d3ccae1
Zoestra <79121105+Zoestra@users.noreply.github.com>
2025-04-03 19:35:18 -0700

    added method comments, finished initial implementation of matrixscanner

--- src/MatrixScanner.py
+++ src/MatrixScanner.py
@@ -1,12 +1,29 @@
 from time import sleep_us
 from rpad import rotopad
-
+from machine import Pin
 
 board = rotopad.profile()
-rows = board[]
+rows = board[0]
+cols = board[1]
+buttons = board[2]
 
 
 def scan_matrix():
+    """Scans through every input and returns a array of activated inputs
+    returns: array of integers representing row/column location of activated input.
+    row 1, column 5 will be indicated as 15
+    10s position is the row, 1s position is column
+    buttons are represented as values < 10
+    """
     inputs = []
+
+    for i in range(len(rows)):
+        rows[i].value(1)
+        for j in range(len(cols)):
+            if cols[j].value() == 1:
+                inputs.append((i + 1) * 10 + (j + 1))
+    for k in range(len(buttons)):
+        if buttons[k] == 1:
+            inputs.append(k + 1)
 
     return inputs
--- src/rpad/rotopad.py
+++ src/rpad/rotopad.py
@@ -1,11 +1,23 @@
 import pinmap_rpad as p
 
+
 def profile():
+    """Returns the rows and columns of this board
+    format:
+    [ rows - [list of Pin objects representing matrix row outputs],
+      cols - [list of Pin objects representing matrix column inputs],
+      buttons - [list of Pin objects representing powered buttons not in the matrix],
+      neopixels - [neopixel data pin, number of pixels],
+      encoders - [[enc1-identifier, button pin / [row, col], np addr / [r,g,b]], [enc2....]]
+    ]
 
-    rows = [p.row1, p.row2, p.row3, p.row4, p.row5] 
+    """
+
+    pixelcount = 35
+
+    rows = [p.row1, p.row2, p.row3, p.row4, p.row5]
     cols = [p.col1, p.col2, p.col3, p.col4, p.col5]
-    buttons    = [p.enc_sw]
-    neopixels  = [p.led_out]
-    encoders = [["rgb", p.enc_sw, [p.enc_led_r, p.enc_led_g, p.enc_led_b]] #[[enc1-identifier, button pin / [row, col], np addr / [r,g,b]], [enc2....]]
-
+    buttons = [p.enc_sw]
+    neopixels = [p.led_out, pixelcount]
+    encoders = [["rgb", p.enc_sw, [p.enc_led_r, p.enc_led_g, p.enc_led_b]]]
     return [rows, cols, buttons, neopixels, encoders]
